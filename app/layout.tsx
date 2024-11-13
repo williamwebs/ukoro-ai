59e2bac6a9cade4fa52385e8f14dcbddcca7f144
William <anazawilliam1@gmail.com>
2024-11-13 13:31:10 +0100

    layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
